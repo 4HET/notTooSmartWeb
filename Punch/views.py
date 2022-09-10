4e8f6ee56cbf2b16c7d6be4eef6113da2cbb005c
4HET <2659335014@qq.com>
2022-09-10 10:08:44 +0800

    ignore

--- Punch/views.py
+++ Punch/views.py
@@ -21,20 +21,6 @@ def postRequest(request):
         # print(ctx['rlt'])
     return render(request, "punch.html", ctx)
 
-def mysql(sql, ty=0):
-    conn = pymysql.connect(host="localhost",user="root", port=3306, passwd="123456", database="android")
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
-    if ty==1:
-        data = cur.fetchall()
-        cur.close()
-        conn.close()
-        return data
-    else:
-        cur.close()
-        conn.close()
-        return
 if __name__ == '__main__':
     sql = 'show tables;'
     print(mysql(sql, 1))
